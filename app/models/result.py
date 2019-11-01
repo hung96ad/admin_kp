792282ce2093f6f74034a14bea756f94827960e6
Citigo <hungnt@ubuntu.ubuntu-domain>
2019-11-01 14:04:12 +0700

    xoa file thua

--- app/models/result.py
+++ app/models/result.py
@@ -3,4 +3,8 @@ class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_election = db.Column(db.Integer, ForeignKey('election.id'))
     image = db.Column(db.String(255))
-    vaild = db.Column(db.Boolean())
+    # 0 chưa làm gì chờ để detect 
+    # 1 đang xử lý 
+    # 2 phiếu hợp lệ
+    # 3 không hợp lệ
+    processed = db.Column(db.Integer)
