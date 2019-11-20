0a8c9341d6177a6d479a824fc58a3926d45a3703
hung96ad <hung96adm@gmail.com>
2019-11-20 13:32:55 +0700

    fix UI/UX

--- app/__init__.py
+++ app/__init__.py
@@ -100,9 +100,9 @@ admin = flask_admin.Admin(
 )
 # print(Election().all())
 # Add model views
-admin.add_view(ElectionView(endpoint='election', menu_icon_type='fa', menu_icon_value='fa-server', name="Xem danh sách các cuộc bầu cử", model=Election, db=db))
-admin.add_view(AddView(name="Thêm cuộc bầu cử", endpoint='add', menu_icon_type='fa', menu_icon_value='fa-calendar-plus-o',))
-admin.add_view(UpdateView(name="Thêm ảnh cuộc bầu cử", endpoint='update', menu_icon_type='fa', menu_icon_value='fa-edit (alias)',))
+admin.add_view(AddView(name="Quản trị bầu cử", endpoint='add', menu_icon_type='fa', menu_icon_value='fa-calendar-plus-o',))
+admin.add_view(ElectionView(endpoint='election', menu_icon_type='fa', menu_icon_value='fa-server', name="Quản lý danh sách các cuộc bầu cử", model=Election, db=db))
+admin.add_view(UpdateView(name="Thống kê cuộc bầu cử", endpoint='update', menu_icon_type='fa', menu_icon_value='fa-edit (alias)',))
 # define a context processor for merging flask-admin's template context into the
 # flask-security views.
 @security.context_processor
