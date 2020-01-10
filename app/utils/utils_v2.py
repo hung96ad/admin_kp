5c25ffc4400b939206f8718be5ab091fa9c69d9d
hung96ad <hung96adm@gmail.com>
2020-01-10 10:22:46 +0700

    fix v2

--- app/utils/utils_v2.py
+++ app/utils/utils_v2.py
@@ -327,7 +327,8 @@ def validate_pre_cell(img, check_num=False):
         segment_one = 0
         segments = get_segment(img_crop, thresh = thresh)
         for i in range(len(segments)):
-            if segments[i]['segment'] > 30 and segments[i]['sum'] <= 2550 and segments[i]['status'] == 1:
+            if segments[i]['segment'] > 30 and segments[i]['sum'] <= 2550 \
+            and segments[i]['status'] == 1 and segments[i+1]['segment'] > 8 and segments[i-1]['segment'] > 8:
                 return 'small', None
             if i != 0 and i!= len(segments)-1:
                 if (segments[i]['segment'] > segments[0]['segment'] \
